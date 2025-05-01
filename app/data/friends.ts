7683f86dfb8cfc80347004e71126dd97e3c97844
mac woohyun <owootak@naver.com>
2025-05-01 19:36:30 +0900

    ✨ Feat mock data 생성

---
+++ app/data/friends.ts
@@ -0,0 +1,46 @@
+const friends = [
+  {
+    id: 1,
+    name: "jamie jones",
+    isOnline: true,
+    profileImage: null,
+  },
+  {
+    id: 2,
+    name: "jake smith",
+    isOnline: true,
+    profileImage: null,
+  },
+  {
+    id: 3,
+    name: "jeff jones",
+    isOnline: true,
+    profileImage: null,
+  },
+  {
+    id: 4,
+    name: "joe smith",
+    isOnline: false,
+    profileImage: null,
+  },
+  {
+    id: 5,
+    name: "daniel jones",
+    isOnline: true,
+    profileImage: null,
+  },
+  {
+    id: 6,
+    name: "rebecca smith",
+    isOnline: false,
+    profileImage: null,
+  },
+  {
+    id: 7,
+    name: "jeff jones",
+    isOnline: false,
+    profileImage: null,
+  },
+];
+
+export default friends;
