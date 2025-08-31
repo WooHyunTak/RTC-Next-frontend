772677f6a8003f8537b093b893483d449d8cacdf
mac woohyun <owootak@onionfive.io>
2025-08-31 15:34:19 +0900

    📝 Modify 채널 추가정보 표기
    각 채널의 추가적인 동작 컨테이너 생성

--- app/components/sub_navs/channels.tsx
+++ app/components/sub_navs/channels.tsx
@@ -1,45 +1,83 @@
 'use client'
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLock, faLockOpen } from "@fortawesome/free-solid-svg-icons";
+import { faEllipsisVertical, faLock, faLockOpen } from "@fortawesome/free-solid-svg-icons";
 import { useQuery } from "@tanstack/react-query";
 import channelsApi from "../../api/channels";
+import { ChannelResponse } from "../../api/channels";
+import { useState } from "react";
+import IconBox from "../Icon_box";
+import CreateChannelModal from "../channels/CreateChannelModal";
 
+function AdditionalInfo({ id }: { id: number }) {
+  const [openCreateChannelModal, setOpenCreateChannelModal] = useState(false);
+  const [channel, setChannel] = useState<ChannelResponse | null>(null);
 
-interface ChannelProps {
-  channel: {
-    id: number;
-    name: string;
-    description: string;
-    isPrivate: boolean;
-    type: string;
-    createdAt: string;
-    updatedAt: string;
+  const getChannel = async () => {
+    const response = await channelsApi.getChannel(id);
+    setChannel(response);
   };
-}
 
-function ChannelItem({ channel }: ChannelProps) {
-  const { name, isPrivate, type } = channel;
-  
+  const handelOpenChannelModal = async (e: React.MouseEvent<HTMLSpanElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    await getChannel();
+    setOpenCreateChannelModal(true);
+  };
 
   return (
-    <div className="flex items-center gap-2 hover:bg-blue-600 p-2 rounded-md">
-      <div className="flex items-center justify-center w-8 h-8 relative">
-        {isPrivate ? (
-          <FontAwesomeIcon icon={faLock} />
-        ) : (
-          <FontAwesomeIcon icon={faLockOpen} />
+    <>
+      <div className="flex flex-col items-center bg-[var(--bg-primary-800)] rounded-md p-2">
+        <span className="text-sm w-full p-2 rounded-md hover:cursor-pointer hover:bg-[var(--bg-primary-700)]" onClick={handelOpenChannelModal} >채널 상세 보기</span>
+        <span className="text-sm w-full p-2 rounded-md hover:cursor-pointer hover:bg-[var(--bg-primary-700)]">채널 삭제</span>
+        <span className="text-sm w-full p-2 rounded-md hover:cursor-pointer hover:bg-[var(--bg-primary-700)]">채널 나가기</span>
+      </div>
+      {openCreateChannelModal && <CreateChannelModal openState={setOpenCreateChannelModal} modalType="update" channelItem={channel} />}
+    </>
+    
+  );
+}
+
+
+function ChannelItem(channel : ChannelResponse) {
+  const { id, name, is_private : isPrivate, type } = channel;
+
+  const [hideAdditionalInfo, setHideAdditionalInfo] = useState(false);
+  const [openAdditionalInfo, setOpenAdditionalInfo] = useState(false);
+
+  return (
+    <>
+      <div className="flex items-center justify-between gap-2 hover:bg-blue-600 p-2 rounded-md"
+        onMouseEnter={() => setHideAdditionalInfo(true)}
+        onMouseLeave={() => setHideAdditionalInfo(false)}
+        >
+        <div className="flex items-center gap-2">
+          <div className="flex items-center justify-center w-8 h-8 ">
+            {isPrivate ? (
+              <FontAwesomeIcon icon={faLock} />
+            ) : (
+              <FontAwesomeIcon icon={faLockOpen} />
+            )}
+          </div>
+          <div className="flex items-center gap-2">
+            <span className="text-sm">{name}</span>
+            {type && (
+              <span className="text-xs ">
+                [{type}]
+              </span>
+            )}
+          </div>
+        </div>
+        {hideAdditionalInfo && (
+          <div className="flex items-center" onClick={() => setOpenAdditionalInfo(!openAdditionalInfo)}>
+            <IconBox name={faEllipsisVertical} position="right"/>
+          </div>
         )}
       </div>
-      <div className="gap-2">
-        <span className="text-sm">{name}</span>
-        {type && (
-          <span className="text-xs ">
-            [{type}]
-          </span>
-        )}
-      </div>
-    </div>
+      {openAdditionalInfo && (
+        <AdditionalInfo id={id} />
+      )}
+    </>
   );
 }
 
@@ -51,8 +89,8 @@ function Channels() {
 
   return (
     <div className="flex flex-col gap-2">
-      {channels?.list.map((channel: ChannelProps["channel"]) => (
-        <ChannelItem key={channel.id} channel={channel} />
+      {channels?.list.map((channel: ChannelResponse) => (
+        <ChannelItem key={channel.id} {...channel} />
       ))}
     </div>
   );
