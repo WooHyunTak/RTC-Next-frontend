ef252a09dfd1ad28fb6e441c25ac732347311170
mac woohyun <owootak@onionfive.io>
2025-08-31 15:35:09 +0900

    📝 Modify 채널 모달 상세 역할을 하도록 재사용
    기존의 생성의 역할만 하던 모달을 상세의 역할도 할 수 있도록 수정

--- app/components/channels/CreateChannelModal.tsx
+++ app/components/channels/CreateChannelModal.tsx
@@ -5,31 +5,40 @@ import Button from "../Button";
 import InputSet from "../Input_set";
 import TextareaSet from "../Textarea_set";
 import ToggleSet from "../Toggle_set";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import channelsApi from "../../api/channels";
+import { ChannelResponse, UpdateChannelRequest } from "../../api/channels";
+
 
 interface CreateChannelModalProps {
     openState: (value: boolean) => void;
+    modalType: "create" | "update";
+    channelItem?: ChannelResponse | null;
 }
 
 interface ChannelParams {
     name: string;
     description: string;
     category: string;
-    isPrivate: boolean;
+    is_private: boolean;
 }
 
 const defaultChannelParams: ChannelParams = {
     name: "",
     description: "",
     category: "",
-    isPrivate: false,
+    is_private: false,
 }
 
 
-function CreateChannelModal({ openState }: CreateChannelModalProps) {
+function CreateChannelModal({ openState, modalType, channelItem }: CreateChannelModalProps) {
+    const queryClient = useQueryClient();
     const [channelParams, setChannelParams] = useState<ChannelParams>({
         ...defaultChannelParams,
     });
+
+    
 
     const handleChangeChannelParams = (key: keyof ChannelParams, value: string | boolean) => {
         setChannelParams({ ...channelParams, [key]: value });
@@ -39,9 +48,50 @@ function CreateChannelModal({ openState }: CreateChannelModalProps) {
         openState(false);
     };  
 
+    const createChannelMutation = useMutation({
+        mutationFn: (params: ChannelParams) => channelsApi.createChannel(params),
+        onSuccess: () => {
+            // queryClient.invalidateQueries({ queryKey: ["my-channels"] });
+            queryClient.refetchQueries({ queryKey: ["my-channels"] });
+            handleClose();
+        },
+        onError: (error) => {
+            console.error(error);
+        }
+    });
+
+    const updateChannelMutation = useMutation({
+        mutationFn: (params: UpdateChannelRequest) => channelsApi.updateChannel(params),
+        onSuccess: () => {
+            queryClient.refetchQueries({ queryKey: ["my-channels"] });
+            handleClose();
+        },
+        onError: (error) => {
+            console.error(error);
+        }
+    });
+
     const handleSaveChannel = () => {
-        console.log(channelParams);
+        if (modalType === "create") {
+            createChannelMutation.mutate(channelParams);
+        } else {
+            updateChannelMutation.mutate({
+                id: channelItem?.id || 0,
+                ...channelParams,
+            });
+        }
     };
+
+    useEffect(() => {
+        if (modalType === "update") {
+            setChannelParams({
+                name: channelItem?.name || "",
+                description: channelItem?.description || "",
+                category: channelItem?.type || "",
+                is_private: channelItem?.is_private || false,
+            });
+        }
+    }, [modalType, channelItem]);
 
     return (
         <ModalTemplate>
@@ -50,26 +100,26 @@ function CreateChannelModal({ openState }: CreateChannelModalProps) {
                     <h1 className="text-2xl font-bold">Create Channel</h1>
                 </header>
                 <div className="flex flex-col gap-2 w-[500px]">
-                    <InputSet label="Name" id="channelName" placeholder="Channel Name" handleChange={
+                    <InputSet label="Name" id="channelName" placeholder="Channel Name" value={channelParams.name} handleChange={
                         (e) => {
                             handleChangeChannelParams("name", e.target.value);
                         }
                     } />
-                    <TextareaSet label="Description" id="channelDescription" placeholder="Channel Description" maxLength={1000} handleChange={(e) => handleChangeChannelParams("description", e.target.value)} />
-                    <InputSet label="Category" id="channelCategory" placeholder="Channel Category" handleChange={
+                    <TextareaSet label="Description" id="channelDescription" placeholder="Channel Description" maxLength={1000} value={channelParams.description} handleChange={(e) => handleChangeChannelParams("description", e.target.value)} />
+                    <InputSet label="Category" id="channelCategory" placeholder="Channel Category" value={channelParams.category} handleChange={
                         (e) => {
                             handleChangeChannelParams("category", e.target.value);
                         }
                     } />
-                    <ToggleSet label="Private channel" size="sm" id="privateChannel" placeholder="Private Channel" handleChange={
+                    <ToggleSet label="Private channel" size="sm" id="privateChannel" placeholder="Private Channel" checked={channelParams.is_private} handleChange={
                         (e) => {
-                            handleChangeChannelParams("isPrivate", e.target.checked);
+                            handleChangeChannelParams("is_private", e.target.checked);
                         }
                     } />
                 </div>
                 <footer className="flex justify-end gap-2">
                     <Button variant="danger" size="md" onClick={handleClose}>Cancel</Button>
-                    <Button variant="primary" size="md" onClick={handleSaveChannel}>Create</Button>                    
+                    <Button variant="primary" size="md" onClick={handleSaveChannel}>{modalType === "create" ? "Create" : "Update"}</Button>                    
                 </footer>
             </div>            
         </ModalTemplate>
