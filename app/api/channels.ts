016f6e9f3f70658ea0c079bd14233d317200e4f9
mac woohyun <owootak@onionfive.io>
2025-08-31 15:33:23 +0900

    📝 Modify 채널 API함수 타입정의

--- app/api/channels.ts
+++ app/api/channels.ts
@@ -1,8 +1,48 @@
 import axiosClient from "./axios_client";
 
-const getMyChannels = async () => {
+export interface CreateChannelRequest {
+  name: string;
+  description: string;
+  category: string;
+  is_private: boolean;
+}
+
+export interface UpdateChannelRequest extends CreateChannelRequest {
+  id: number;
+}
+export interface ChannelResponse {
+    id: number;
+    name: string;
+    description: string;
+    is_private: boolean;
+    type: string;
+    createdAt: string;
+    updatedAt: string;
+}
+
+export interface ChannelResponseList {
+  list: ChannelResponse[];
+}
+
+const getMyChannels = async () : Promise<ChannelResponseList> => {
   const response = await axiosClient.get("/api/channels/my-channels/");
   return response.data;
 };
 
-export default { getMyChannels };
+const createChannel = async (data: CreateChannelRequest) : Promise<ChannelResponse> => {
+  const response = await axiosClient.post("/api/channels/", data);
+  return response.data;
+};
+
+const getChannel = async (id: number) : Promise<ChannelResponse> => {
+  const response = await axiosClient.get(`/api/channels/${id}/`);
+  return response.data;
+};
+
+const updateChannel = async (data: UpdateChannelRequest) : Promise<ChannelResponse> => {
+  const response = await axiosClient.post(`/api/channels/${data.id}/`, data);
+  return response.data;
+};
+
+
+export default { getMyChannels, createChannel, updateChannel, getChannel };
